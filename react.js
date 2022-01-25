167e3ea3f25e7a47ac858abf45be453c8fbdf932
Ritesh Nemade <ritesh@eumentis.com>
2022-01-25 14:47:53 +0530

    eslint-airbnb and eslint-airbnb-typescript configs added;

---
+++ react.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "airbnb",
+    "airbnb/hooks",
+    "airbnb-typescript",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:eslint-comments/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:promise/recommended",
+    "prettier",
+  ],
+};
